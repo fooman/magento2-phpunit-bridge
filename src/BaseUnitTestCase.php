d15a2e12b754daee51a093b88e4103881d85c25b
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2018-07-11 00:28:56 +1200

    Formatting

--- src/BaseUnitTestCase.php
+++ src/BaseUnitTestCase.php
@@ -5,24 +5,24 @@ namespace Fooman\PhpunitBridge;
 class BaseUnitTestCase extends CompatTestCase
 {
 
-	protected function createMock($originalClassName)
-	{
-		if (is_callable('parent::createMock')) {
-			return parent::createMock($originalClassName);
-		} 
+    protected function createMock($originalClassName)
+    {
+        if (is_callable('parent::createMock')) {
+            return parent::createMock($originalClassName);
+        }
 
         return $this->getMockBuilder($originalClassName)
                     ->disableOriginalConstructor()
                     ->disableOriginalClone()
                     ->disableArgumentCloning()
                     ->getMock();
-	}
+    }
 
-	protected function createPartialMock($originalClassName, array $methods)
-	{
-		if (is_callable('parent::createPartialMock')) {
-			return parent::createPartialMock($originalClassName, $methods);
-		} 
+    protected function createPartialMock($originalClassName, array $methods)
+    {
+        if (is_callable('parent::createPartialMock')) {
+            return parent::createPartialMock($originalClassName, $methods);
+        }
 
         return $this->getMockBuilder($originalClassName)
                     ->disableOriginalConstructor()
@@ -30,5 +30,5 @@ class BaseUnitTestCase extends CompatTestCase
                     ->disableArgumentCloning()
                     ->setMethods(empty($methods) ? null : $methods)
                     ->getMock();
-	}
+    }
 }
