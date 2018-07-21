8140c569cdbbfd9203ed144e2ea42ce72e82f3d1
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2018-07-21 23:20:14 +1200

    Use reflection to determine what Magento requires, then retrofit via class aliasing and providing compatibility methods

--- src/BaseUnitTestCase.php
+++ src/BaseUnitTestCase.php
@@ -5,6 +5,75 @@ namespace Fooman\PhpunitBridge;
 class BaseUnitTestCase extends CompatTestCase
 {
 
+    public function getMock(
+        $originalClassName,
+        $methods = [],
+        array $arguments = [],
+        $mockClassName = '',
+        $callOriginalConstructor = true,
+        $callOriginalClone = true,
+        $callAutoload = true,
+        $cloneArguments = false,
+        $callOriginalMethods = false,
+        $proxyTarget = null
+    ) {
+        if (is_callable('parent::getMock')) {
+            return parent::getMock(
+                $originalClassName,
+                $methods,
+                $arguments,
+                $mockClassName,
+                $callOriginalConstructor,
+                $callOriginalClone,
+                $callAutoload,
+                $cloneArguments,
+                $callOriginalMethods,
+                $proxyTarget
+            );
+        }
+        //TODO only first 5 arguments are dealt with
+        return $this->useMockBuilderToCreateMock(
+            $originalClassName,
+            $methods,
+            $arguments,
+            $mockClassName,
+            $callOriginalConstructor
+        );
+    }
+
+    public function getMockForAbstractClass(
+        $originalClassName,
+        array $arguments = [],
+        $mockClassName = '',
+        $callOriginalConstructor = true,
+        $callOriginalClone = true,
+        $callAutoload = true,
+        $mockedMethods = [],
+        $cloneArguments = false
+    ) {
+        if (is_callable('parent::getMockForAbstractClass')) {
+            return parent::getMockForAbstractClass(
+                $originalClassName,
+                $arguments,
+                $mockClassName,
+                $callOriginalConstructor,
+                $callOriginalClone,
+                $callAutoload,
+                $mockedMethods,
+                $cloneArguments
+            );
+        }
+
+        //TODO only 5 arguments are dealt with
+        return $this->useMockBuilderToCreateMock(
+            $originalClassName,
+            $mockedMethods,
+            $arguments,
+            $mockClassName,
+            $callOriginalConstructor
+        );
+    }
+
     protected function createMock($originalClassName)
     {
         if (is_callable('parent::createMock')) {
@@ -30,5 +99,26 @@ class BaseUnitTestCase extends CompatTestCase
                     ->disableArgumentCloning()
                     ->setMethods(empty($methods) ? null : $methods)
                     ->getMock();
+    }
+
+    private function useMockBuilderToCreateMock(
+        $originalClassName,
+        $methods,
+        array $arguments,
+        $mockClassName,
+        $callOriginalConstructor
+    ) {
+        $builder = $this->getMockBuilder($originalClassName)
+                        ->setMethods($methods)
+                        ->setConstructorArgs($arguments)
+                        ->setMockClassName($mockClassName);
+
+        if ($callOriginalConstructor) {
+            $builder->enableOriginalConstructor();
+        } else {
+            $builder->disableOriginalConstructor();
+        }
+
+        return $builder->getMock();
     }
 }
--- src/CompatTestCase.php
+++ src/CompatTestCase.php
@@ -2,14 +2,26 @@
 
 namespace Fooman\PhpunitBridge;
 
-if (method_exists(\PHPUnit\Runner\Version::class, 'id')
-    && version_compare(\PHPUnit\Runner\Version::id(), '6.0.0', '>=')
-) {
+$reflection = new \ReflectionClass(\Magento\Framework\TestFramework\Unit\Helper\ObjectManager::class);
+if (!$reflection) {
+    throw new \Exception('Can\'t find Magento Test Framework ObjectManager - is this a Magento 2 project?');
+}
+$type = $reflection->getConstructor()->getParameters()[0]->getType();
+
+if ($type === \PHPUnit\Framework\TestCase::class) {
+    if (!class_exists(\PHPUnit\Framework\TestCase::class)) {
+        class_alias(\PHPUnit_Framework_TestCase::class, \PHPUnit\Framework\TestCase::class);
+    }
+
     class CompatTestCase extends \PHPUnit\Framework\TestCase
     {
 
     }
 } else {
+    if (!class_exists(\PHPUnit_Framework_TestCase::class)) {
+        class_alias(\PHPUnit\Framework\TestCase::class, \PHPUnit_Framework_TestCase::class);
+    }
+
     class CompatTestCase extends \PHPUnit_Framework_TestCase
     {
 
