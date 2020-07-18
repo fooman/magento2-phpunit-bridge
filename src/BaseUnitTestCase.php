f92caffec292eab29cf1f7274145f60a2022a1eb
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2020-07-18 22:22:46 +1200

    backport assertStringContainsString

--- src/BaseUnitTestCase.php
+++ src/BaseUnitTestCase.php
@@ -6,6 +6,14 @@ use PHPUnit\Framework\MockObject\MockObject;
 
 class BaseUnitTestCase extends CompatTestCase
 {
+    public static function assertStringContainsString(string $needle, string $haystack, string $message = ''): void
+    {
+        if (is_callable('parent::assertStringContainsString')) {
+            parent::assertStringContainsString($needle, $haystack, $message);
+        } else {
+            parent::assertContains($needle, $haystack, $message);
+        }
+    }
 
     private function useMockBuilderToCreateMock(
         $originalClassName,
